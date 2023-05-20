64e2314520b19be9657cbd1f8a6737589a2eec07
bham1710 <bhamlivesforever1710@gmaill.com>
2023-05-20 17:54:21 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+
+import {HomePage} from './HomePage';
+import {Apple} from './Apple';
+import {NotFound} from './NotFound';
+
+const App = () =>  (
+  <BrowserRouter>
+    <div>
+      <Link
+        to= "/"
+        style={{marginLeft: 5,
+        }}
+      >
+        Home
+      </Link>
+      <Link 
+        to= "/apple"
+        style={{marginLeft: 5,
+        }}
+      > 
+        Apple
+      </Link>  
+      <Link 
+        to= "/applet"
+        style={{marginLeft: 5,
+        }}
+        > Applet </Link>
     </div>
-  );
-}
+    <Routes>
+      <Route path="/"element={<HomePage/>}></Route>
+      <Route path="/apple"element={<Apple/>}></Route>
+      <Route path="*"element={<NotFound/>}></Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/Apple.js
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+export const Apple = () => {
+
+    const Navigate = useNavigate();
+
+    const onHomePageButtonClick = () => {
+        Navigate("/");
+    };
+    
+    return(
+        <div>
+            <div> Apple Page </div>
+            <button onclick={onHomePageButtonClick}>Navigate to Home Page</button>
+        </div>
+    );
+};
